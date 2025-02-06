5d748a32d7f519aff2e596257cc6b7962f050bb5
vipin018 <vipinpathak018@gmail.com>
2025-02-06 20:57:43 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useFrame  } from '@react-three/fiber'
 import { Box } from '@react-three/drei'
 import { DirectionalLight, MeshStandardMaterial } from 'three'
 import { OrbitControls } from '@react-three/drei'
+import { useRef } from 'react'
 
 const Cube = ({position,size,color}) => {
+const ref = useRef()
+  useFrame((state,delta) => {
+    ref.current.rotation.x += delta
+  })
+
   return (
-    <mesh position={position}>
+    <mesh position={position} ref={ref}>
       <boxGeometry args={size}/>
       <meshStandardMaterial
         color={color}
@@ -19,14 +25,16 @@ const App = () => {
   return (
     <Canvas>
       <OrbitControls/>
-      {/* <directionalLight position={[0,0,2]}/> */}
-      <ambientLight intensity={0.8}/>
-      <group  position={[-1,-1,1]}>
+      <directionalLight position={[0,0,2]}/>
+      {/* <ambientLight intensity={0.8}/> */}
+<Cube position={[0,0,0]} size={[1,1,1]} color={"blue"} />
+
+      {/* <group  position={[-1,-1,1]}>
       <Cube position={[0,0,0]} size={[1,1,1]} color={"blue"}/>
       <Cube position={[0,2,0]} size={[1,1,1]} color={"red"}/>
       <Cube position={[3,0,0]} size={[1,1,1]} color={"green"}/>
       <Cube position={[3,2,0]} size={[1,1,1]} color={"yellow"}/>
-      </group>
+      </group> */}
       </Canvas>
   )
 }
