5e17c39d43ccac12b67701fecb9032da4e925ff7
vipin018 <vipinpathak018@gmail.com>
2025-02-06 21:35:32 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
-import React from 'react'
-import { Canvas, useFrame  } from '@react-three/fiber'
-import { Box } from '@react-three/drei'
+import React, { useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Box, Sphere } from '@react-three/drei'
 import { DirectionalLight, MeshStandardMaterial } from 'three'
 import { OrbitControls } from '@react-three/drei'
 import { useRef } from 'react'
+import { SkyGeometry } from 'three/examples/jsm/Addons.js'
 
-const Cube = ({position,size,color}) => {
-const ref = useRef()
-  useFrame((state,delta) => {
-    ref.current.rotation.x += delta
+const Cube = ({ position, size, color }) => {
+  const ref = useRef()
+
+  const [hovered, setHovered] = useState(false)
+  const [clicked, setClicked] = useState(false)
+  const [scale, setScale] = useState(1)
+  useFrame((state, delta) => {
+    const speed = hovered ? 10 : 0.5
+    ref.current.rotation.y += delta * speed;
+    // ref.current.position.z += Math.sin(state.clock.getElapsedTime()) / 20
   })
 
   return (
-    <mesh position={position} ref={ref}>
-      <boxGeometry args={size}/>
+    <mesh
+      position={position}
+      ref={ref}
+      onPointerEnter={(event) => (event.stopPropagation(), setHovered(true))} 
+      onPointerLeave={(event) => (event.stopPropagation(), setHovered(false))}
+      onClick={(event) => (event.stopPropagation(), setClicked(!clicked))}
+      scale={clicked ? 2.5 : 1}
+      onPointerDown={(event) => (event.stopPropagation(), setScale(2.5))}
+      onPointerUp={(event) => (event.stopPropagation(), setScale(1))}
+      >
+      <sphereGeometry args={size} />
       <meshStandardMaterial
         color={color}
+        wireframe={true}
+        
       />
     </mesh>
   )
@@ -24,10 +42,10 @@ const ref = useRef()
 const App = () => {
   return (
     <Canvas>
-      <OrbitControls/>
-      <directionalLight position={[0,0,2]}/>
-      {/* <ambientLight intensity={0.8}/> */}
-<Cube position={[0,0,0]} size={[1,1,1]} color={"blue"} />
+      <OrbitControls />
+      {/* <directionalLight position={[0,0,2]}/> */}
+      <ambientLight intensity={0.8} />
+      <Cube position={[0, 0, 0]} size={[1]} color={"burlywood"} />
 
       {/* <group  position={[-1,-1,1]}>
       <Cube position={[0,0,0]} size={[1,1,1]} color={"blue"}/>
@@ -35,7 +53,7 @@ const App = () => {
       <Cube position={[3,0,0]} size={[1,1,1]} color={"green"}/>
       <Cube position={[3,2,0]} size={[1,1,1]} color={"yellow"}/>
       </group> */}
-      </Canvas>
+    </Canvas>
   )
 }
 
