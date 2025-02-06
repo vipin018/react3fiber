338ef3b8e224da449a33ab2afa3f649c2b743ecc
vipin018 <vipinpathak018@gmail.com>
2025-02-06 16:18:43 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Box } from '@react-three/drei'
+import { CylinderGeometry, MeshBasicMaterial, MeshStandardMaterial } from 'three'
+import { OrbitControls } from '@react-three/drei'
+
+const Cube = ({position,size,color}) => {
+  return (
+    <mesh position={position}>
+      <boxGeometry args={size}/>
+      <meshBasicMaterial
+        color={color}
+      />
+    </mesh>
+  )
+}
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Canvas>
+      <Cube position={[1,2,0]} size={[2,1,1]} color={"blue"}/>
+      </Canvas>
   )
 }
 
