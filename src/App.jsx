8ae7d938fb46986b3a220c94c234707900af0aa9
vipin018 <vipinpathak018@gmail.com>
2025-02-06 16:28:11 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import { Box } from '@react-three/drei'
-import { CylinderGeometry, MeshBasicMaterial, MeshStandardMaterial } from 'three'
-import { OrbitControls } from '@react-three/drei'
+import { DirectionalLight, MeshStandardMaterial } from 'three'
 
 const Cube = ({position,size,color}) => {
   return (
     <mesh position={position}>
       <boxGeometry args={size}/>
-      <meshBasicMaterial
+      <meshStandardMaterial
         color={color}
       />
     </mesh>
@@ -18,7 +17,9 @@ const Cube = ({position,size,color}) => {
 const App = () => {
   return (
     <Canvas>
-      <Cube position={[1,2,0]} size={[2,1,1]} color={"blue"}/>
+      <directionalLight position={[0,0,2]}/>
+      <Cube position={[0,0,0]} size={[2,1,1]} color={"blue"}/>
+      <Cube position={[0,2,0]} size={[2,1,1]} color={"red"}/>
       </Canvas>
   )
 }
