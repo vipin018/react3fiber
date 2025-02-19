391a50e6b851abe452e11ff701b61e8e497c9267
vipin018 <vipinpathak018@gmail.com>
2025-02-20 00:40:21 +0530

    updated

--- random-1/src/App.jsx
+++ random-1/src/App.jsx
@@ -1,47 +1,51 @@
-import React from 'react'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import { Environment } from '@react-three/drei'
-import Rotate from './Rotate'
-const App = () => {
-  return (
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Environment } from "@react-three/drei";
+import Rotate from "./Rotate";
+import { Leva, useControls } from "leva";
 
-    <div className='w-full text-white h-full bg-gray-900'>
-      <div className=' w-full h-full'>
-        <img className='w-full h-full object-cover object-center' src="https://images.unsplash.com/photo-1524502397800-2eeaad7c3fe5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fG1vZGVsfGVufDB8fDB8fHwy" alt="" />
+const App = () => {
+  // Leva GUI Controls
+  const { showGrid, gridSize, environment } = useControls("Scene Settings", {
+    showGrid: true,
+    gridSize: { value: 10, min: 5, max: 50, step: 1 },
+    environment: { options: ["studio", "sunset", "forest", "city"] },
+  });
+
+  return (
+    <div className="w-full text-white h-full bg-gray-900">
+      <Leva collapsed /> {/* Leva Panel */}
+      <div className="w-full h-full">
+        <img
+          className="w-full h-full object-cover object-center"
+          src="https://images.unsplash.com/photo-1524502397800-2eeaad7c3fe5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fG1vZGVsfGVufDB8fDB8fHwy"
+          alt=""
+        />
       </div>
-      <div className='w-1/2 h-full bg-blue-800'>
-        <Canvas className='w-full h-full'>
-          {/* <mesh>
-          <boxGeometry args={[2, 2, 2]} />
-          <meshStandardMaterial
-            roughness={0}
-            metalness={1}
-            toneMapped={true} // this will make the scene look more realistic it has to be true or if it is false the scene will look like a cartoon
-          />
-          <OrbitControls />
-        </mesh> */}
-          <Environment preset="studio" />
+
+      {/* First Canvas - Rotating Cube with Dynamic Environment */}
+      <div className="w-1/2 h-full bg-blue-800">
+        <Canvas className="w-full h-full">
+          <Environment preset={environment} />
           <Rotate />
+          {showGrid && <gridHelper args={[gridSize, gridSize]} />}
         </Canvas>
-        <div className='w-full h-full bg-green-800 flex justify-center items-center'>
-          <Canvas className='w-full h-full'>
+
+        {/* Second Canvas - Static Cube */}
+        <div className="w-full h-full bg-green-800 flex justify-center items-center">
+          <Canvas className="w-full h-full">
             <Environment preset="sunset" />
             <mesh>
               <boxGeometry args={[2, 2, 2]} />
-              <meshStandardMaterial
-
-                toneMapped={true}
-                metalness={0}
-                roughness={1}
-              />
+              <meshStandardMaterial toneMapped={true} metalness={0} roughness={1} />
             </mesh>
             <OrbitControls />
+            {showGrid && <gridHelper args={[gridSize, gridSize]} />}
           </Canvas>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
