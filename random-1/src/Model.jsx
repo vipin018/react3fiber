a8c66923142862399aa26b7cbe5226421537d70c
vipin018 <vipinpathak018@gmail.com>
2025-02-21 00:28:58 +0530

    updated

--- random-1/src/Model.jsx
+++ random-1/src/Model.jsx
@@ -4,22 +4,22 @@ import { useFrame } from "@react-three/fiber";
 import { useControls } from "leva";
 
 const Model = () => {
-  const { scene } = useGLTF("/glow.glb"); // Load 3D model
+  const { scene } = useGLTF("/portal.glb"); // Load 3D model
   const modelRef = useRef(); // Reference for rotation
 
   // 🔄 Animate rotation
   useFrame(({ clock }) => {
     if (modelRef.current) {
-      modelRef.current.rotation.y = clock.getElapsedTime(); // Adjust speed if needed
+    //   modelRef.current.rotation.z = Math.cos(clock.getElapsedTime()*2); // Adjust speed if needed
     }
   });
 
   // ✅ Position & Scale Controls using Leva
   const { x, y, z, scale } = useControls("Model Settings", {
     x: { value: 0, min: -10, max: 10, step: 0.1, label: "X Position" },
-    y: { value: 0, min: -10, max: 10, step: 0.1, label: "Y Position" },
+    y: { value: -2, min: -10, max: 10, step: 0.1, label: "Y Position" },
     z: { value: 0, min: -10, max: 10, step: 0.1, label: "Z Position" },
-    scale: { value: 1.5, min: 0.1, max: 5, step: 0.1, label: "Scale" },
+    scale: { value: 5, min: 0.1, max: 50, step: 0.1, label: "Scale" },
   });
 
   return (
